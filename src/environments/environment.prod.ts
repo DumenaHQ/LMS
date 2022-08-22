8ee58ea140b3d1f4bc61c3729222c02d69c99638
David <dheyved1@gmail.com>
2022-08-22 13:13:36 +0100

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dev.dumena.com/',
+  baseUrl: 'https://api.dumena.com/',
   paystackKey: 'pk_test_d586eb9d6d6ec62439571f56fea7c8330e4d4366',
 };
