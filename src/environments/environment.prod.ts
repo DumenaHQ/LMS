2b2daeaf95b5e5562bafb9dcaff0abccd3bdeb7d
David <dheyved1@gmail.com>
2022-08-31 12:41:59 +0100

    formatted payment amount

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.dumena.com/',
+  baseUrl: 'https://prod.dumena.com/',
   paystackKey: 'pk_live_651e4ffb3b25a40cec00a89618ff55c62d5cfae6',
 };
