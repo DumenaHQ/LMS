efd231b884b909f1239a9ec773a2ab8d37235f51
David <dheyved1@gmail.com>
2022-10-27 13:27:10 +0100

    made changes to the home page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://api.dumena.com/',
   paystackKey: 'pk_live_651e4ffb3b25a40cec00a89618ff55c62d5cfae6',
+  // livePaystackKey: 'pk_live_651e4ffb3b25a40cec00a89618ff55c62d5cfae6',
 };
