58f2473cd1876bfbb87ecf0256ddac23242ce873
David <dheyved1@gmail.com>
2022-12-08 02:03:41 +0100

    validate minlength

--- src/app/modules/school/components/school-overview/school-overview.component.ts
+++ src/app/modules/school/components/school-overview/school-overview.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { SchoolService } from 'src/app/services/school.service';
 
 @Component({
   selector: 'app-school-overview',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./school-overview.component.scss']
 })
 export class SchoolOverviewComponent implements OnInit {
+  user: any;
+  dataLoading: boolean = true;
+  students: any;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private schoolService: SchoolService
+  ) {}
 
   ngOnInit(): void {
+    // Get User data from localstorage
+    let userData = this.authService.getUser();
+    this.user = userData.user;
+
+    // Get school learners from localstorage
+    this.schoolService.getSchoolLearners(this.user.id).subscribe({
+      next: (res: any) => {
+        this.students = res.data.students;
+      },
+      error: (e) => console.error(e),
+    });
   }
 
 }
