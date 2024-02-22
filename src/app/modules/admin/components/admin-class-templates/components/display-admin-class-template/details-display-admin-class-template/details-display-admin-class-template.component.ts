eff95735d644fb468a33d03f95cad83d9b180c72
busola <busola.omosipe@gmail.com>
2024-02-22 13:10:57 +0100

    Added class template details

--- src/app/modules/admin/components/admin-class-templates/components/display-admin-class-template/details-display-admin-class-template/details-display-admin-class-template.component.ts
+++ src/app/modules/admin/components/admin-class-templates/components/display-admin-class-template/details-display-admin-class-template/details-display-admin-class-template.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ClassroomService } from 'src/app/services/classroom.service';
+import { ClassTemplateDetailModel } from './interfaces/class-template.model';
 
 @Component({
   selector: 'app-details-display-admin-class-template',
@@ -6,10 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details-display-admin-class-template.component.scss']
 })
 export class DetailsDisplayAdminClassTemplateComponent implements OnInit {
+  
+  contentId: any = 'courses';
+  currentProgramId: any;
+  classroomTemplate!: ClassTemplateDetailModel;
+  dataLoading: boolean = true;
+  addCourseToProgram: boolean = false;
 
-  constructor() { }
+  constructor(
+    // private programsService: ProgramsService,
+    private classroomService: ClassroomService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private changeDectetorRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
+    // Get Current Program
+    this.currentProgramId = this.activatedRoute.snapshot.params;
+
+    // Get programs
+    this.classroomService
+      .getClassroomTemplateById(this.currentProgramId.classTemplateId)
+      .subscribe({
+        next: (res: any) => {
+          this.classroomTemplate = res.data.classTemplate;
+          this.changeDectetorRef.detectChanges();
+        },
+        error: (e) => console.error(e),
+        // complete: () => {
+        //   this.dataLoading = false;
+        // },
+      });
   }
 
+  // Open add course to program modal
+  openAddCourseToProgramModal() {
+    this.addCourseToProgram = true
+  }
+
+  // Close add course to program modal
+  closeAddCourseToProgramModal() {
+    this.addCourseToProgram = false
+  }
+
+  // Edit program
+  editProgram() {
+    this.router.navigate([`/admin/programs/${this.currentProgramId.programId}/edit-program`]);
+  }
+
+  // Tab change
+  tabChange(ids: any) {
+    this.contentId = ids;
+  }
 }
+
