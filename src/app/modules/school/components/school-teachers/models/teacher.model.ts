58db8991349974e993af657746a2158ccb85ae1d
busola <busola.omosipe@gmail.com>
2024-03-28 19:29:00 +0100

    Added Fetch teachers and create teacher

---
+++ src/app/modules/school/components/school-teachers/models/teacher.model.ts
@@ -0,0 +1,9 @@
+export interface TeacherlModel {
+    schoolID?:        string;
+    createdAt?:       Date;
+    updatedAt?:       Date;
+    id?:              string;
+    fullname?:        string;
+    email?:           string;
+    isUserOnboarded?: boolean;
+}
