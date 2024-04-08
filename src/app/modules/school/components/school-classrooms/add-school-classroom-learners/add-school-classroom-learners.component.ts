8031c2af8f7c628856a322d4f63bf6263c33e1fa
David <dheyved1@gmail.com>
2024-04-08 11:26:28 +0100

    filter learner when adding learner to classroom

--- src/app/modules/school/components/school-classrooms/add-school-classroom-learners/add-school-classroom-learners.component.ts
+++ src/app/modules/school/components/school-classrooms/add-school-classroom-learners/add-school-classroom-learners.component.ts
@@ -29,9 +29,25 @@ export class AddSchoolClassroomLearnersComponent implements OnInit {
   learnersList: any;
   schoolLearners: any;
   parentLearners: any;
-  dataLoading: boolean = true;
+  dataLoading: boolean = false;
   studentName: any;
   selectedLearners: any[] = [];
+
+  grades: any = [
+    { id: '', name: 'All Grades' },
+    { id: 1, name: 'Grade 1' },
+    { id: 2, name: 'Grade 2' },
+    { id: 3, name: 'Grade 3' },
+    { id: 4, name: 'Grade 4' },
+    { id: 5, name: 'Grade 5' },
+    { id: 6, name: 'Grade 6' },
+    { id: 7, name: 'Grade 7' },
+    { id: 8, name: 'Grade 8' },
+    { id: 9, name: 'Grade 9' },
+    { id: 10, name: 'Grade 10' },
+    { id: 11, name: 'Grade 11' },
+    { id: 12, name: 'Grade 12' },
+  ]
   
 
   constructor(
@@ -45,31 +61,22 @@ export class AddSchoolClassroomLearnersComponent implements OnInit {
     let userData = this.authService.getUser();
     this.user = userData.user;
 
-    // Get school learners from localstorage
-    if(this.user.role === 'school') {
-      this.schoolService.getSchoolLearners(this.user.id, undefined).subscribe({
-        next: (res: any) => {
-          this.schoolLearners = res.data.students;
-        },
-        error: (e) => console.error(e),
-        complete: () => {
-          this.dataLoading = false;
-        },
-      });
-    } else {
-      // Get parent learners from localstorage
-      this.authService.getParentChildren(this.user.id).subscribe({
-        next: (res: any) => {
-          this.parentLearners = res.data.learners;
-          console.log(this.parentLearners);
-        },
-        error: (e) => console.error(e),
-        complete: () => {
-          this.dataLoading = false;
-        },
-      });
-    }
+    this.getAllStudents(undefined);
+  }
 
+  // Get all students
+  getAllStudents(event: any) {
+    this.dataLoading = true;
+    let grade = event?.target?.value === '' ? undefined : event?.target?.value;
+    this.schoolService.getSchoolLearners(this.user.id, grade).subscribe({
+      next: (res: any) => {
+        this.schoolLearners = res.data.students;
+      },
+      error: (e) => console.error(e),
+      complete: () => {
+        this.dataLoading = false;
+      }
+    });
   }
 
   // Tab change
