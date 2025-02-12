978f3a1d973b97bb94fb687b21e4e9c83f4fe926
Okolo Chibuzo <chibuzo.henry@gmail.com>
2025-02-12 16:59:32 +0100

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'http://52.91.5.88:3000/',
+    // https://documenter.getpostman.com/view/Uz5Gnw2i?version=latest#7beed2e5-8916-4f4e-8339-088483a52cd0
+};
