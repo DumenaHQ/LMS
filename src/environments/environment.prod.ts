99f845079116c4b73579af2140fd79221087918a
Okolo Chibuzo <chibuzo.henry@gmail.com>
2025-02-12 18:36:16 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://52.91.5.88:3000/',
+    baseUrl: 'https://dev.dumena.com/',
     // https://documenter.getpostman.com/view/Uz5Gnw2i?version=latest#7beed2e5-8916-4f4e-8339-088483a52cd0
 };
